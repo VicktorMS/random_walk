3a1a993e1273aaa1d44ee92d29ccee71c01d6325
victormoraes <victor.moraesrj@gmail.com>
2024-04-21 15:29:37 -0300

    Retactored random_walk class

--- random_walk.py
+++ random_walk.py
@@ -2,27 +2,20 @@ from random import choice
 
 
 class RandomWalk():
-    def __init__(self, num_points=5000):
+    def __init__(self, num_points=5000, walk_distance=list(range(0, 5))):
         self.num_points = num_points
         
         # Starting value is always 0,0
         self.x_values = [0]
         self.y_values = [0]
-        self.walk_distance = list(range(0, 5))
+        self.walk_distance = walk_distance
 
     def fill_walk(self):
         # Keep steps until walk reaches the desired value
         while len(self.x_values) < self.num_points:
             
-            # Decide which direction to go and how far to go in that direction
-
-            x_direction = choice([1, -1]) # Randomly choose aif the value is positive (right) or negative (left)
-            x_distance = choice(self.walk_distance) # Randomly choose the travel distance
-            x_step = x_direction * x_distance
-            
-            y_direction = choice([1, -1]) 
-            y_distance = choice(self.walk_distance) 
-            y_step = y_direction * y_distance
+            x_step = self.get_step()
+            y_step = self.get_step()
 
             if x_step == 0 and y_step == 0:
                 continue
@@ -34,4 +27,11 @@ class RandomWalk():
             self.x_values.append(next_x)
             self.y_values.append(next_y)
 
-            
+    
+    def get_step(self):
+        # Decide which direction to go and how far to go in that direction
+
+        direction = choice([1, -1]) # Randomly choose aif the value is positive (right) or negative (left)
+        distance = choice(self.walk_distance) # Randomly choose the travel distance
+        return direction * distance
+
--- rw_visual.py
+++ rw_visual.py
@@ -3,10 +3,14 @@ import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
 while True:
-    rw = RandomWalk()
+    rw = RandomWalk(num_points=5000, walk_distance=list(range(0, 5)))
     rw.fill_walk()
 
-    plt.scatter(rw.x_values, rw.y_values, c='red', s=15)
+    plt.figure(dpi=128, figsize=(10, 6))
+    
+    plt.scatter(rw.x_values, rw.y_values, c="red", s=1)
+    #plt.plot(rw.x_values, rw.y_values, color='red', linewidth=1)
+
     plt.show()
 
     keep_running = input('Create another walk? (Y/N)')
